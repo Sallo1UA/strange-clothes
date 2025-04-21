65c5ce04fa60378b1ba3ce0b78b31f4866466b03
Sallo_Ua <78312706+Sallo1UA@users.noreply.github.com>
2025-04-21 21:04:43 +0300

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,5 @@
+let toggleThemeBtn = document.querySelector(".toggle-theme");
+toggleThemeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    toggleThemeBtn.textContent = document.body.classList.contains("dark") ? '☀️' : '🌙';
+})
