2384f7ef350fe9e2c3a49edf85486fb7d070cc5f
Sallo_Ua <78312706+Sallo1UA@users.noreply.github.com>
2025-04-21 21:06:17 +0300

    Delete strange-clothes directory

--- strange-clothes/theme.js
+++
@@ -1,5 +0,0 @@
-let toggleThemeBtn = document.querySelector(".toggle-theme");
-toggleThemeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    toggleThemeBtn.textContent = document.body.classList.contains("dark") ? '☀️' : '🌙';
-})
